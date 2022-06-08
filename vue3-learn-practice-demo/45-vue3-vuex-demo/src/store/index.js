634b0a0f8d7230eda99615eadbda5d5452833aab
高原 <gaoyuan200408@credithc.com>
2022-06-08 15:50:26 +0800

    feat:vuex数据状态的保持以及更改操作

--- vue3-learn-practice-demo/45-vue3-vuex-demo/src/store/index.js
+++ vue3-learn-practice-demo/45-vue3-vuex-demo/src/store/index.js
@@ -1,9 +1,29 @@
 import { createStore } from 'vuex'
-
+// VUex 数据管理框架
+// Vuex  创建了一个全局唯一的仓库，用来存放全局的数据
 export default createStore({
-  state: {},
+  state: {
+    name: '孙悟空',
+  },
   getters: {},
-  mutations: {},
-  actions: {},
+  // 建议：mutations 只能写同步代码，不允许写异代码
+  // commit 和 mutation 做关联
+  mutations: {
+    // 第四步：对应的 mutation 会被执行
+    change(state, payload) {
+      // 第五步： 在此处修改数据
+      console.log(state, payload)
+      state.name = payload
+    },
+  },
+  // dispatch 和 actions 做关联
+  actions: {
+    // 第二步：store 感知到你触发了一个叫做 change 的 action, 执行 change 方法
+    change(context, payload) {
+      console.log(1221)
+      // 第三步，提交一个 commit 触发一个 mutation
+      this.commit('change', payload)
+    },
+  },
   modules: {},
 })
