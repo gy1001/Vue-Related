bec236e9ee3b1495c8e8b09f7459f5e46c81fde2
高原 <gaoyuan200408@credithc.com>
2022-06-10 18:53:43 +0800

    feat:flex + iconfont 完成首页 docker 样式编写

--- vue3-learn-practice-demo/46-jingdong/src/App.vue
+++ vue3-learn-practice-demo/46-jingdong/src/App.vue
@@ -1,29 +1,60 @@
 <template>
-  <nav>
+  <div class="docker">
+    <!-- BEM 命名规则
+      block_element Modifier
+     -->
+    <span class="docker_item docker_item--active">
+      <i class="iconfont">&#xe752;</i>
+      <div class="docker_item-title">首页</div>
+    </span>
+    <span class="docker_item">
+      <i class="iconfont">&#xe64e;</i>
+      <div class="docker_item-title">购物车</div>
+    </span>
+    <span class="docker_item">
+      <i class="iconfont">&#xe600;</i>
+      <div class="docker_item-title">订单</div>
+    </span>
+    <span class="docker_item">
+      <i class="iconfont">&#xe63c;</i>
+      <div class="docker_item-title">我的</div>
+    </span>
+  </div>
+  <!-- <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  </nav> -->
+  <!-- <router-view /> -->
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style lang="scss" scoped>
+.docker {
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 0.49rem;
+  border-top: solid 1px #f1f1f1;
+  background-color: lightblue;
   text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+  display: flex;
+  padding: 0 0.18rem;
+  box-sizing: border-box;
+  &_item {
+    &--active {
+      color: #1fa4fc;
+    }
+    flex: 1;
+    &-title {
+      font-size: 0.2rem; // 低于12px 浏览器不会再处理，所以要进行特殊处理
+      transform-origin: center top;
+      transform: scale(0.5, 0.5);
+    }
+    .iconfont {
+      font-size: 0.18rem;
+      margin-top: 0.07rem;
+      margin-bottom: 0.02rem;
+      display: inline-block;
     }
   }
 }
