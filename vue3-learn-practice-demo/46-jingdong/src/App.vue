8db3d42d1cd3733ba57a263c650e3a814634e8f4
MineMine <gaoyuan_flying@163.com>
2022-06-11 23:26:31 +0800

    feat:使用Scss 组织地址区域布局

--- vue3-learn-practice-demo/46-jingdong/src/App.vue
+++ vue3-learn-practice-demo/46-jingdong/src/App.vue
@@ -1,4 +1,11 @@
 <template>
+  <div class="wrapper">
+    <div class="position">
+      <span class="position__icon iconfont">&#xe636;</span>
+      山西理工大学国防科技园2号楼10层 北京北京北京北京北京北京
+      <span class="position__notice iconfont">&#xe63d;</span>
+    </div>
+  </div>
   <div class="docker">
     <!-- BEM 命名规则
       block_element Modifier
@@ -28,18 +35,48 @@
 </template>
 
 <style lang="scss" scoped>
+@import './style/variable.scss';
+@import './style/mixins.scss';
+.wrapper {
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: 0.49rem;
+  right: 0;
+  padding: 0 0.18rem;
+  .position {
+    @include ellipse;
+    padding: 0.16rem 0;
+    line-height: 0.22rem;
+    font-size: 0.16rem;
+    position: relative;
+    padding-right: 0.24rem;
+    color: $content-font-color;
+    &__icon {
+      top: 1rem;
+      margin-right: 0.08rem;
+    }
+    &__notice {
+      position: absolute;
+      font-size: 0.2rem;
+      top: 0.17rem;
+      right: 0;
+    }
+  }
+}
 .docker {
   position: absolute;
   left: 0;
   bottom: 0;
   width: 100%;
   height: 0.49rem;
-  border-top: solid 1px #f1f1f1;
+  border-top: solid 0.01rem #f1f1f1;
   background-color: lightblue;
   text-align: center;
   display: flex;
   padding: 0 0.18rem;
   box-sizing: border-box;
+  color: $content-font-color;
   &_item {
     &--active {
       color: #1fa4fc;
