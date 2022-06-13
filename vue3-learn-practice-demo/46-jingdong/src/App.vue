8a002b9bcaa682fa843c3f34fbd936a960ae5477
MineMine <gaoyuan_flying@163.com>
2022-06-13 16:00:25 +0800

    feat:利用CSS技巧实现搜索及 banner 区域布局

--- vue3-learn-practice-demo/46-jingdong/src/App.vue
+++ vue3-learn-practice-demo/46-jingdong/src/App.vue
@@ -17,7 +17,17 @@
         src="http://www.dell-lee.com/imgs/vue3/banner.jpg"
       />
     </div>
-    <span>122</span>
+    <div class="icons">
+      <div class="icons__item" v-for="item in navList" :key="item.desc">
+        <img
+          :src="`http://www.dell-lee.com/imgs/vue3/${item.imgName}.png`"
+          alt=""
+          class="icons__item-icon"
+        />
+        <p class="icons__item-desc">{{ item.desc }}</p>
+      </div>
+    </div>
+    <div class="border-line"></div>
   </div>
   <div class="docker">
     <!-- BEM 命名规则
@@ -47,6 +57,27 @@
   <!-- <router-view /> -->
 </template>
 
+<script>
+import { defineComponent, reactive } from 'vue'
+export default defineComponent({
+  setup() {
+    const navList = reactive([
+      { imgName: '超市', desc: '超市便利' },
+      { imgName: '菜市场', desc: '菜市场' },
+      { imgName: '水果店', desc: '水果店' },
+      { imgName: '鲜花', desc: '鲜花绿植' },
+      { imgName: '医药健康', desc: '医药健康' },
+      { imgName: '家居', desc: '家居时尚' },
+      { imgName: '蛋糕', desc: '烘培蛋糕' },
+      { imgName: '签到', desc: '签到' },
+      { imgName: '大牌免运', desc: '大牌免运' },
+      { imgName: '红包', desc: '红包套餐' }
+    ])
+    return { navList }
+  }
+})
+</script>
+
 <style lang="scss" scoped>
 @import './style/variable.scss';
 @import './style/mixins.scss';
@@ -64,7 +95,7 @@
     font-size: 0.16rem;
     position: relative;
     padding-right: 0.24rem;
-    color: $content-font-color;
+    color: $content-fontColor;
     &__icon {
       top: 1rem;
       margin-right: 0.08rem;
@@ -108,6 +139,33 @@
     border-bottom: 25%;
   }
 }
+.icons {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  margin-top: 0.16rem;
+  color: $content-fontColor;
+  &__item {
+    width: 20%;
+    &-icon {
+      display: block;
+      width: 0.4rem;
+      height: 0.4rem;
+      margin: 0 auto;
+    }
+    &-desc {
+      text-align: center;
+      margin: 0.06rem 0 0.16rem 0;
+      font-size: 0.12rem;
+    }
+  }
+}
+.border-line {
+  margin: 0 -0.18rem;
+  height: 0.1rem;
+  background: $content-bgColor;
+}
 .docker {
   position: absolute;
   left: 0;
@@ -120,7 +178,8 @@
   display: flex;
   padding: 0 0.18rem;
   box-sizing: border-box;
-  color: $content-font-color;
+  color: $content-fontColor;
+  background-color: $bgColor;
   &_item {
     &--active {
       color: #1fa4fc;
