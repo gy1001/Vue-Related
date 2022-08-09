2f70123e681fc27f759ce85bbef59a9f7a666850
高原 <gaoyuan200408@credithc.com>
2022-08-09 16:06:12 +0800

    feat:renderTemplate完成

--- vue-mustache/src/index.js
+++ vue-mustache/src/index.js
@@ -3,6 +3,7 @@ import MyMustache from './my-mustache/index.js'
 var templateStr = `
   <div>
       <div class="mine">{{name}}</div>
+      <div >总成绩为：{{a.b.c}}分</div>
       <ol id="me" style="color: red">
         {{#students}}
           <li>
@@ -18,8 +19,19 @@ var templateStr = `
     </div>
   `
 var data = {
-  thing: '华为手机',
-  mood: '开心',
+  name: '我是三年二班',
+  a: { b: { c: 1000 } },
+  students: [
+    { name: '小明', hobbies: ['游泳', '健身'] },
+    { name: '小哄', hobbies: ['足球', '篮球', '羽毛球'] },
+    { name: '小强', hobbies: ['吃饭', '睡觉', '打豆豆'] },
+  ],
 }
+
+// var templateStr = `我买了一个{{thing}},好{{mood}}啊`
+// var data = {
+//   thing: '华为手机',
+//   mood: '开心',
+// }
 
 const result = MyMustache.render(templateStr, data)
--- vue-mustache/src/my-mustache/renderTemplate.js
+++ vue-mustache/src/my-mustache/renderTemplate.js
@@ -1,3 +1,58 @@
-const renderTemplate = () => {}
+/**
+ * 处理数组，结合 renderTemplate 实现递归，
+ * 注意：这里函数接受的参数是 token 而不是 tokens
+ * token 就类似于一个简单的 ['#', "students", [ ...]] 中的 第 2 个索引的值
+ *
+ * @param {*} token  []
+ * @param {*} array []
+ */
+function parseArray(token, array) {
+  let htmlStr = ''
+  array.forEach((item) => {
+    htmlStr += renderTemplate(token, { ...item, '.': item })
+  })
+  return htmlStr
+}
+
+const renderTemplate = (tokens, data) => {
+  let htmlStr = ''
+  for (let index = 0; index < tokens.length; index++) {
+    const token = tokens[index]
+    switch (token[0]) {
+      case 'text':
+        htmlStr += token[1]
+        break
+      case 'name':
+        // 这里存在多个 . 的情况，暂不处理
+        htmlStr += lookUp(token[1], data)
+        // htmlStr += data[token[1]]
+        break
+      case '#':
+        // ['#',"students", [...]]
+        htmlStr += parseArray(token[2], data[token[1]])
+        break
+      default:
+        break
+    }
+  }
+  return htmlStr
+}
+
+/**
+ * 功能是可以在 dataObj 对象中，寻找用连续点符号的 keyName 属性
+ * 比如 dataObj是 {a:{b:{c:100}}}
+ * 那么 lookUp('a.b.c', dataObj) 结果就是100
+ */
+function lookUp(keyName, data) {
+  if (keyName !== '.' && keyName.indexOf('.') !== -1) {
+    let temp = data
+    const keysArr = keyName.split('.')
+    keysArr.forEach((key) => {
+      temp = temp[key]
+    })
+    return temp
+  }
+  return data[keyName]
+}
 
 export default renderTemplate
