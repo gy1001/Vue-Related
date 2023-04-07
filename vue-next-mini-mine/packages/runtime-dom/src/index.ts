591c0742a76e9bdd549c9dad1d4608db88052459
MineMine <gaoyuan_flying@163.com>
2023-04-07 14:02:54 +0800

    完善createApp

--- vue-next-mini-mine/packages/runtime-dom/src/index.ts
+++ vue-next-mini-mine/packages/runtime-dom/src/index.ts
@@ -1,4 +1,4 @@
-import { extend } from '@vue/shared'
+import { extend, isString } from '@vue/shared'
 import { createRenderer } from '@vue/runtime-core'
 import { nodeOps } from './nodeOps'
 import { patchProp } from './patchProp'
@@ -14,5 +14,21 @@ export const render = (...args) => {
 
 export const createApp = (...args) => {
   const app = ensureRender().createApp(...args)
+  const { mount } = app
+  app.mount = (containerOrSelector: Element | string) => {
+    const container = noramlizeContainer(containerOrSelector)
+    if (!container) {
+      console.log('容器必须存在')
+      return
+    }
+    mount(container)
+  }
   return app
+}
+function noramlizeContainer(container: Element | string): Element {
+  if (isString(container)) {
+    const res = document.querySelector(container)
+    return res as Element
+  }
+  return container
 }
