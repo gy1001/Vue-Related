138be5f41cbf7a03894c1833d3bf2a04807dd599
MineMine <gaoyuan_flying@163.com>
2023-04-27 15:44:10 +0800

    高级特性：生命周期函数hooks注册

--- imooc-build/lib/service/Service.js
+++ imooc-build/lib/service/Service.js
@@ -11,9 +11,9 @@ class Service {
     this.hooks = {}
     // this.dir = process.cwd()
   }
-  start() {
-    console.log('启动服务')
-    this.resolveConfig()
+  async start() {
+    await this.resolveConfig()
+    this.registerHooks()
   }
   // 解析配置文件
   async resolveConfig() {
@@ -44,11 +44,29 @@ class Service {
         configParams = await import(configPath)
         configParams = configParams.default
       }
-      console.log(configParams)
+      this.config = configParams
     } else {
       console.log('配置文件不存在，终止执行')
       process.exit(1)
     }
+  }
+
+  // 注册钩子函数
+  registerHooks() {
+    log.verbose('解析hooks')
+    // hooks 数据结构 [["int", function()],"success", function(){}]
+    const { hooks } = this.config
+    hooks.forEach((hook) => {
+      const [key, fn] = hook
+      if (key && fn && typeof key === 'string' && typeof fn === 'function') {
+        const existHook = this.hooks[key]
+        if (!existHook) {
+          this.hooks[key] = []
+        }
+        this.hooks[key].push(fn)
+      }
+    })
+    log.verbose('hooks', this.hooks)
   }
 }
 
--- imooc-build/samples/imooc-build.config.mjs
+++ imooc-build/samples/imooc-build.config.mjs
@@ -3,6 +3,20 @@ import path from 'path'
 export default {
   entry: path.isAbsolute(entry) ? entry : path.resolve(entry),
   plugins: [],
+  hooks: [
+    [
+      'created',
+      function () {
+        console.log('created')
+      },
+    ],
+    [
+      'configResolved',
+      function () {
+        console.log('configResolved')
+      },
+    ],
+  ],
   // output: path.resolve('dist'),
 }
 
