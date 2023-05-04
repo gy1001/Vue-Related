e51907020d1d3505ede7861978e9b19fd4aba8f1
MineMine <gaoyuan_flying@163.com>
2023-05-04 11:07:42 +0800

    webpack初始配置mode开发

--- imooc-build/plugins/initPlugin/index.js
+++ imooc-build/plugins/initPlugin/index.js
@@ -1,3 +1,9 @@
 module.exports = function initPlugin(api, params) {
   console.log('init plugin')
+  const { getWebpackConfig } = api
+  const config = getWebpackConfig()
+  // 获取构建模式
+  const mode = process.env.IMOOC_BUILD_MODE || 'development'
+  config.mode(mode)
+  console.log('mode', config.toConfig())
 }
