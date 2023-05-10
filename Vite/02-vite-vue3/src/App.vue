4d954279336f48700cd58c84bebc9080fb8158bd
MineMine <gaoyuan_flying@163.com>
2023-05-10 17:51:02 +0800

    vite支持vue3项目开发

---
+++ Vite/02-vite-vue3/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="test">
+    this is vite page
+    <div class="test-2">this is test2</div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+@import './index.less';
+</style>
