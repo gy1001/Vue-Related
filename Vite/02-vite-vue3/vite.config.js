b9a4e2c807a847090ec51dd93ca26597fa6b6470
MineMine <gaoyuan_flying@163.com>
2023-05-10 22:53:55 +0800

    如何让vite打包代码支持低版本浏览器运行？

--- Vite/02-vite-vue3/vite.config.js
+++ Vite/02-vite-vue3/vite.config.js
@@ -1,5 +1,12 @@
 import { defineConfig } from 'vite'
 import pluginVue from '@vitejs/plugin-vue'
+import legacyPlugin from '@vitejs/plugin-legacy'
+
 export default defineConfig({
-  plugins: [pluginVue()],
+  plugins: [
+    pluginVue(),
+    legacyPlugin({
+      targets: ['defaults', 'not IE 11'],
+    }),
+  ],
 })
