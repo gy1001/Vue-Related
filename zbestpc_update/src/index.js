c9c71b18d0626243142b8a19f9f76621a320bc61
MineMine <gaoyuan_flying@163.com>
2023-05-11 11:08:19 +0800

    删除项目

--- zbestpc_update/src/index.js
+++
@@ -1,6 +0,0 @@
-import './css/public.css'
-import './css/index.css'
-
-import 'jquery'
-import './js/public'
-import './js/nav'
--- zbestpc_update/src/mpa/login.js
+++
@@ -1,3 +0,0 @@
-import { createApp } from 'vue'
-import Login from '../Login.vue'
-createApp(Login).mount('#app')
