b0c848886809cd98ceb9c497ffcd57bbb80cb190
MineMine <gaoyuan_flying@163.com>
2023-07-03 14:39:06 +0800

    项目初始化

---
+++ mini-vue-router/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const count = ref(100)
+</script>
+
+<template>
+  <div @click="count++">
+    {{ count }}
+  </div>
+</template>
+
+<style scoped></style>
