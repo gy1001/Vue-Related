b1ca13a6e40d2024b7af83dba9d94cf6d19b3f4e
MineMine <gaoyuan_flying@163.com>
2024-09-18 20:01:55 +0800

    feat:05-解析模板参数

---
+++ vue2-stage/src/compiler/index.js
@@ -0,0 +1,6 @@
+// 对模板进行编译处理
+export function compilerToFunction(template) {
+  console.log(template)
+  // 1 就是将 template 转换为 AST 语法树
+  // 2. 生成 render 方法，（render 方法执行返回的结果就是 虚拟 DOM）
+}
--- vue2-stage/src/init.js
+++ vue2-stage/src/init.js
@@ -1,5 +1,5 @@
 import { initState } from './state'
-
+import { compilerToFunction } from './compiler/index'
 // 就是给 Vue 添加 Init 方法
 export function initMixin(Vue) {
   Vue.prototype._init = function (options) {
@@ -12,5 +12,33 @@ export function initMixin(Vue) {
 
     // 初始化状态
     initState(vm)
+
+    if (options.el) {
+      vm.$mount(options.el)
+    }
+  }
+
+  Vue.prototype.$mount = function (el) {
+    const vm = this
+    el = document.querySelector(el)
+    const options = vm.$options
+    // 先查找有没有 render 函数
+    if (!options.render) {
+      // 没有写模板 但是写了 el
+      if (!options.template && el) {
+        options.template = el.outerHTML
+      }
+    }
+
+    if (options.template) {
+      // 这里需要对模板进行编译
+      const render = compilerToFunction(options.template)
+      options.render = render // 最终可以获取 render 方法
+      // script 标签使用的 vue.global.js 这个编译过程是在浏览器中运行的
+
+      // runtime 是不包含模板编译的，整个编译过程是打包的过程中和通过 loader 来转译 .vue 文件，用runtime 的时候不能使用 template 属性
+    }
+
+    // 最终就可以获取 render 函数
   }
 }
